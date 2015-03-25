e83193ac156aa070e43df0624fe1ffc767de7685
Ollie Edwards <olliee@mobbu.com>
2015-03-25 20:47:31 +0000

    select before assign

--- packages/resource-pool/ResourcePool.js
+++ packages/resource-pool/ResourcePool.js
@@ -1,28 +1,62 @@
 ResourcePool = function(config, players) {
     config = config || resourceConfig;
-    var limit = config.resourceLimit[players] || 7,
-        pool = {
-            assigned : {},
-            assign: function(player, resources) {
-                resources = Array.isArray(resources) ? resources : [resources];
-                var assigned = pool.assigned[player] || [];
-                resources.forEach(function(resource) {
-                    var count = pool[resource];
-                     if (count > 0) {
-                         pool[resource] = count - 1;
-                         assigned.push(resource);
-                     }
-                });
-                pool.assigned[player] = assigned;
+    var limit = config.resourceLimit[players] || 7;
+
+    var pool = (function(){
+        var pool = {};
+        (config.concreteResources || []).forEach(function(e) {
+            pool[e] = limit;
+        });
+        (config.wildResources || []).forEach(function(e) {
+            pool[e] = ( config.resourceLimit[e] || 7);
+        });
+        return pool;
+    }());
+
+    var ownership = {},
+        sale = [];
+
+    var selector;
+
+    var self = {
+        available: pool,
+        startSelection: function(player) {
+            self.release(sale);
+            sale = [];
+            selector = player;
+        },
+        select: function(resource) {
+            if (!selector) {
+                throw "No active selector";
             }
-        };
+            var count = pool[resource];
+            if (count > 0) {
+                pool[resource] = count - 1;
+                sale.push(resource);
+            }
+            self.sale = sale;
+        },
+        deselect: function(resource) {
 
-    (config.concreteResources || []).forEach(function(e) {
-        pool[e] = limit;
-    });
-    (config.wildResources || []).forEach(function(e) {
-        pool[e] = ( config.resourceLimit[e] || 7);
-    });
+        },
+        assign: function() {
+            if (!selector) {
+                throw "No active selector";
+            }
 
-    return pool;
+            var assigned = ownership[selector] || [];
+            sale.forEach(function(resource) {
+                assigned.push(resource);
+            });
+            sale = [];
+            ownership[selector] = assigned;
+        },
+        release: function(resources) {
+
+        },
+        assigned: ownership,
+        sale: sale
+    }
+
+    return self;
 }
--- packages/resource-pool/resource-pool-tests.js
+++ packages/resource-pool/resource-pool-tests.js
@@ -6,29 +6,37 @@ var limit2 = 4,
     limit3 = 5;
 
 var config = {
-  "resourceLimit" : {
-    "2" : limit2,
-    "3" : limit3,
-    "gold" : 5
-},
-  "concreteResources" : [red,green],
-  "wildResources" : [gold]
+    "resourceLimit": {
+        "2": limit2,
+        "3": limit3,
+        "gold": 5
+    },
+    "concreteResources": [red, green],
+    "wildResources": [gold]
 }
 
 
 Tinytest.add('resource numbering - 2/3 players', function (test) {
-  for (var i=2; i<=3; i++) {
-    var pool = ResourcePool(config, i);
-    test.equal(pool[red], config.resourceLimit["" + i]);
-    test.equal(pool[green], config.resourceLimit["" + i]);
-    test.equal(pool[gold], config.resourceLimit[gold]);
-  }
+    for (var i = 2; i <= 3; i++) {
+        var pool = ResourcePool(config, i);
+        test.equal(pool.available[red], config.resourceLimit["" + i]);
+        test.equal(pool.available[green], config.resourceLimit["" + i]);
+        test.equal(pool.available[gold], config.resourceLimit[gold]);
+    }
 });
 
-Tinytest.add('resource assignment - single', function(test) {
-  var pool = ResourcePool(config, 2);
-  test.equal(Object.keys(pool.assigned).length, 0);
-  pool.assign("player1", red);
-  test.equal(Object.keys(pool.assigned).length, 1);
-  test.equal(pool[red], 3);
+Tinytest.add('resource selection and assignment - single', function (test) {
+    var pool = ResourcePool(config, 2);
+
+    pool.startSelection("player1");
+    test.equal(pool.sale.length, 0);
+    test.equal(Object.keys(pool.assigned).length, 0);
+
+    pool.select(red);
+    test.equal(Object.keys(pool.assigned).length, 0, "assigned resources on selection");
+    test.equal(pool.sale.length, 1);
+    test.equal(pool.available[red], 3);
+
+    pool.assign();
+    test.equal(pool.assigned["player1"], [red]);
 });
